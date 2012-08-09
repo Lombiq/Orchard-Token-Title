2633acad9643b715cead3d269fa3476263a96b16
Piedone <piedone@pyrocenter.hu>
2012-08-09 16:49:39 +0200

    Adding missing StringLength attribute to record

--- Models/TokenTitlePartRecord.cs
+++ Models/TokenTitlePartRecord.cs
@@ -1,9 +1,11 @@
 ﻿using Orchard.ContentManagement.Records;
+using System.ComponentModel.DataAnnotations;
 
 namespace Piedone.TokenTitle.Models
 {
     public class TokenTitlePartRecord : ContentPartRecord
     {
+        [StringLength(1024)]
         public virtual string TitlePattern { get; set; }
     }
 }
